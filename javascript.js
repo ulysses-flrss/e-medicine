31aaad91bcff367f81d9b3a4bff228ce6a4ae75a
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-07-28 23:27:58 -0600

    update index

--- javascript.js
+++ javascript.js
@@ -1,15 +1,47 @@
+$(document).ready(function () {
 
-    var nav = document.getElementsByClassName(".nav");
-if (nav){
-    nav[0].addEventListener("click", hoverIn, false);
-    alert("SIU")
-} else {
-    alert("nou");
-}
+    $("#li-cita").hover(function() {
+        $("#cita").attr("src", "imgs/document (1).png");
+    }, function () {
+        $("#cita").attr("src", "imgs/document.png");
+    })
 
 
-function hoverIn () {
-    console.log ('EL WIDTH SE HA CAMBIADO');
-    nav[0].style.width =  600;
+    $("#li-doctor").hover(function() {
+        $("#doctor").attr("src", "imgs/doctor(1).png");
+    }, function () {
+        $("#doctor").attr("src", "imgs/doctor.png");
+    })
+
+    $("#li-familiares").hover(function() {
+        $("#familiares").attr("src", "imgs/family.png");
+    }, function () {
+        $("#familiares").attr("src", "imgs/group.png");
+    })
     
-}
+    $("#li-faq").hover(function() {
+        $("#faq").attr("src", "imgs/faq (1).png");
+    }, function () {
+        $("#faq").attr("src", "imgs/faq.png");
+    })
+
+    $("#li-about").hover(function() {
+        $("#about").attr("src", "imgs/information(2).png");
+    }, function () {
+        $("#about").attr("src", "imgs/information.png");
+    })
+
+    $("#li-comentarios").hover(function() {
+        $("#comentarios").attr("src", "imgs/comments.png");
+    }, function () {
+        $("#comentarios").attr("src", "imgs/comment.png");
+    })
+
+
+    $("#li-logout").hover(function() {
+        $("#logout").attr("src", "imgs/sign-out.png");
+    }, function () {
+        $("#logout").attr("src", "imgs/logout.png");
+    })
+    
+})
