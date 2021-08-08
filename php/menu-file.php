958c144ef1453157e809c50bab2338994bb90f45
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-08-07 22:18:17 -0600

    update

---
+++ php/menu-file.php
@@ -0,0 +1,58 @@
+<!--INICIO DEL MENÚ-->
+<nav class="nav">
+        <ul class="navbar-nav" id="nav">
+            <li class="nav-item">
+                <img src="logoEMedicine.png" alt="" class="logo">
+            </li>
+
+            <li class="nav-item" id="li-cita">
+                <a href="#" class="nav-link" >
+                    <img src="imgs/document.png" alt="" class="nav-picture" id="cita">
+                    <span class="link-text">Programar Cita</span>
+                </a>
+            </li>
+
+            <li class="nav-item" id="li-familiares">
+                <a href="#" class="nav-link" >
+                    <img src="imgs/group.png" alt="" class="nav-picture" id="familiares">
+                    <span class="link-text">Familiares</span>
+                </a>
+            </li>
+
+            <li class="nav-item" id="li-doctor">
+                <a href="#" class="nav-link">
+                    <img src="imgs/doctor.png" alt="" class="nav-picture" id="doctor">
+                    <span class="link-text">Especialidades Medicas</span>
+                </a>
+            </li>
+
+            <li class="nav-item" id="li-faq">
+                <a href="#" class="nav-link">
+                    <img src="imgs/faq.png" alt="" class="nav-picture" id="faq">
+                    <span class="link-text">Preguntas Frecuentes</span>
+                </a>
+            </li>
+
+            <li class="nav-item" id="li-comentarios">
+                <a href="#" class="nav-link">
+                    <img src="imgs/comment.png" alt="" class="nav-picture" id="comentarios">
+                    <span class="link-text">Comentarios</span>
+                </a>
+            </li>
+
+            <li class="nav-item" id="li-about">
+                <a href="#" class="nav-link" id="li-info">
+                    <img src="imgs/information.png" alt="" class="nav-picture" id="about">
+                    <span class="link-text">Acerca de Nosotros</span>
+                </a>
+            </li>
+
+            <li class="nav-item" id="li-logout">
+                <a href="#" class="nav-link">
+                    <img src="imgs/logout.png" alt="" class="nav-picture" id="logout">
+                    <span class="link-text">Cerrar Sesión</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+<!--FIN DEL MENÚ-->
