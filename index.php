41b692de26483e84d8e3a029002301b96e6e6008
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-08-16 15:07:07 -0600

    update

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
     <main class="allContainer">
         <div class="bienvenida" id="container-bienvenida">
             <a href="#" class="imagen-texto">
-                <div class="img-layout"><img src="psd.jpg" alt="Imagen de Perfil" class="profile-picture" title="Imagen de Perfil"></div>
+                <div class="img-layout"><img src="imagenPrueba.png" alt="Imagen de Perfil" class="profile-picture" title="Imagen de Perfil"></div>
                 <span class="editar" title="Editar Perfil">Editar Perfil</span>
             </a>
-                <span id="bienvenida" title="Bienvenida">Bienvenido/a Ulises</span>
+                <span id="bienvenida" title="Bienvenida">Bienvenido/a Usuario</span>
         </div>
         
 
