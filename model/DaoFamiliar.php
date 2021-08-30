94609f591844e0090694bafbc4538644504af763
ulysses-flrss <66712163+ulysses-flrss@users.noreply.github.com>
2021-08-30 15:43:13 -0600

    Add files via upload

--- model/DaoFamiliar.php
+++ model/DaoFamiliar.php
@@ -7,7 +7,7 @@ require_once("../model/classConexion.php");
             
             $cn = new Conexion();
             $dbh = $cn->getConexion();
-            $sql = "INSERT INTO familiar (nombre, peso, altura, fechaNac, genero, municipio, enfermedades) VALUES (:nombre, :peso, :altura, :fechaNac, :genero, :municipio, :enfermedades)";
+            $sql = "INSERT INTO familiar (id, nombre, peso, altura, fechaNac, genero, municipio, enfermedades) VALUES (:id, :nombre, :peso, :altura, :fechaNac, :genero, :municipio, :enfermedades)";
 
             try {
                 $stmt = $dbh->prepare($sql);
@@ -20,5 +20,76 @@ require_once("../model/classConexion.php");
             } catch (PDOException $e) {
                 echo "ERROR:" . $e->getMessage();
             }
-        }}
+        }
+
+        public function modificar($familiar){
+            $cn = new Conexion();        
+            $dbh = $cn->getConexion();
+            $sql = "UPDATE familiar SET nombre=:nombre,  peso=:peso, altura=:altura, fechaNac=:fechaNac, genero=:genero, municipio=:municipio, enfermedades=:enfermedades";
+            try{
+                $stmt = $dbh->prepare($sql);
+                $stmt->bindParam(':nombre',$familiar->nombre);
+                $stmt->bindParam(':peso',$familiar->peso);
+                $stmt->bindParam(':altura',$familiar->altura);
+                $stmt->bindParam(':fechaNac',$familiar->fechaNac);
+                $stmt->bindParam(':genero',$familiar->genero);
+                $stmt->bindParam(':municipio',$familiar->municipio);
+                $stmt->bindParam(':enfermedades',$familiar->enfermedades);
+                
+    
+                $stmt->execute();
+            }catch(PDOException $e){
+                echo "Error: " . $e->getMessage();
+            }
+        } 
+
+ 
+
+        
+      
+
+
+        public function eliminar($familiar){
+            $cn = new Conexion;        
+            $dbh = $cn->getConexion();
+            $sql = "DELETE FROM familiar WHERE id=:id";
+
+            try{
+                $stmt = $dbh->prepare($sql);
+               
+                $stmt->execute();    
+            }catch(PDOException $e){
+                echo "Error: " . $e->getMessage();
+            }
+        } 
+
+
+        public function listadoFamiliar(){
+            $sql = "SELECT id, nombre FROM familiar ORDER BY id";
+            $cn = new Conexion();
+            $dbh = $cn->getConexion();
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute();
+            $familiar = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $familiar;
+        }
+
+    public function mostrarFamiliar() {
+        $sql = "SELECT * FROM familiar";
+        $cn = new Conexion;
+        $dbh = $cn->getConexion();
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $familiar = $stmt->fetch();
+        return $familiar;
+
+
+            
+            
+    
+
+    }
+    
+    
+    }
 ?>
