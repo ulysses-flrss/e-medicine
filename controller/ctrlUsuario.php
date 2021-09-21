6bcad9794b6bbfa612a93f3ee10f0cbff2851243
Guillermo-31 <guillermogr211@gmail.com>
2021-09-21 10:27:27 -0600

    Update ctrlUsuario.php

--- controller/ctrlUsuario.php
+++ controller/ctrlUsuario.php
@@ -1,30 +1,76 @@
 <?php
   // Notificar todos los errores excepto E_NOTICE
   error_reporting(E_ALL ^ E_NOTICE);
+    session_start();
 
-  function username(){
+  function username($idUsuario){
     require_once '../model/classConexion.php';
-    $idUsuario = $_SESSION['codUsuario'];
+    
+    if (isset($idUsuario)) {
+      $idUsuario = $_SESSION['codUsuario'];
 
-    $cn = new Conexion();
-    $dbh = $cn->getConexion();
-    $sql = "SELECT idPaciente, CONCAT(nombre, ' ', apellido) FROM pacientes WHERE idPaciente=:idUsuario";
-    $stmt = $dbh->prepare($sql);
-    $stmt->bindParam(':idUsuario', $idUsuario);
-    $stmt->execute();
-    $resultado = $stmt->fetch();
-    if ($resultado == true) {
-      $dataUser = $resultado[0].'/'.$resultado[1];
-      return $dataUser;
-    }else{
-      $sql = "SELECT CONCAT(nombre, ' ', apellido) FROM doctores WHERE idDoctor=:idUsuario";
+      $cn = new Conexion();
+      $dbh = $cn->getConexion();
+      $sql = "SELECT idPaciente, CONCAT(nombre, ' ', apellido) FROM pacientes WHERE idPaciente=:idUsuario";
       $stmt = $dbh->prepare($sql);
       $stmt->bindParam(':idUsuario', $idUsuario);
       $stmt->execute();
       $resultado = $stmt->fetch();
-      if (isset($resultado)) {
-        $nombreUsuario = $resultado[0];
-        return $nombreUsuario;
+      if ($resultado == true) {
+        $dataUser = $resultado[0].'/'.$resultado[1].'/paciente';
+        return $dataUser;
+      }else{
+        $sql = "SELECT idDoctor, CONCAT(nombre, ' ', apellido) FROM doctores WHERE idDoctor=:idUsuario";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':idUsuario', $idUsuario);
+        $stmt->execute();
+        $resultado = $stmt->fetch();
+        if ($resultado == true) {
+          $dataUser = $resultado[0].'/'.$resultado[1].'/doctor';
+          return $dataUser;
+        }else{
+          $sql = "SELECT idAdmin, CONCAT(nombre, ' ', apellido) FROM admins WHERE idAdmin=:idUsuario";
+          $stmt = $dbh->prepare($sql);
+          $stmt->bindParam(':idUsuario', $idUsuario);
+          $stmt->execute();
+          $resultado = $stmt->fetch();
+          if ($resultado == true) {
+            $dataUser = $resultado[0].'/'.$resultado[1].'/admin';
+            return $dataUser;
+          }
+        }//FIN IF RESULTADO DOCTOR
+      }
+    }else{
+      $cn = new Conexion();
+      $dbh = $cn->getConexion();
+      $sql = "SELECT idPaciente, CONCAT(nombre, ' ', apellido) FROM pacientes WHERE idPaciente=:idUsuario";
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindParam(':idUsuario', $idUsuario);
+      $stmt->execute();
+      $resultado = $stmt->fetch();
+      if ($resultado == true) {
+        $dataUser = $resultado[0].'/'.$resultado[1].'/paciente';
+        return $dataUser;
+      }else{
+          $sql = "SELECT idDoctor, CONCAT(nombre, ' ', apellido) FROM doctores WHERE idDoctor=:idUsuario";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':idUsuario', $idUsuario);
+        $stmt->execute();
+        $resultado = $stmt->fetch();
+        if ($resultado == true) {
+          $dataUser = $resultado[0].'/'.$resultado[1].'/doctor';
+          return $dataUser;
+        }else{
+          $sql = "SELECT idAdmin, CONCAT(nombre, ' ', apellido) FROM admins WHERE idAdmin=:idUsuario";
+          $stmt = $dbh->prepare($sql);
+          $stmt->bindParam(':idUsuario', $idUsuario);
+          $stmt->execute();
+          $resultado = $stmt->fetch();
+          if ($resultado == true) {
+            $dataUser = $resultado[0].'/'.$resultado[1].'/admin';
+            return $dataUser;
+          }
+        }//FIN IF RESULTADO DOCTOR
       }
     }
   }
