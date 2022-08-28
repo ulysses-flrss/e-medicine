0cce0910e19511587f112febbb2903fabf45fac1
carlosLGM <100099447+carlosLGM@users.noreply.github.com>
2022-08-28 13:14:45 -0600

    Update ctrlWelcome.php

--- controller/ctrlWelcome.php
+++ controller/ctrlWelcome.php
@@ -1,14 +1,16 @@
+
 <?php
 
 error_reporting(E_ALL ^ E_NOTICE);
 session_start();
+
 
 $accion = isset($_REQUEST['accion'])?$_REQUEST['accion']:""; 
         
 
 if ($accion == "invitado") {
     require_once "../view/viewInvitado.php";
-} elseif ($accion == "inicioSesion") {
+} else if ($accion == "inicioSesion") {
     require_once "../view/viewLogin.php";
 } else {
     require_once "../view/viewInvitado.php";
